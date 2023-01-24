b91f3f70b0e0bf610a43b4b950563763a17fe302
Wvvern <84735487+Wvvern@users.noreply.github.com>
2023-01-23 18:32:12 -0800

    FileLogger.cs implementation

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Logger;
+
+internal class FileLogger : BaseLogger
+{
+    private string _filePath;
+
+    public FileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public override void Log(LogLevel logLevel, string message)
+    {
+        FileStream stream = new(_filePath, FileMode.Append);
+        StreamWriter writer = new(stream);
+        writer.WriteLine($"{DateTime.Now} {ClassName} {logLevel}: {message}");
+        writer.Dispose();
+    }
+}
