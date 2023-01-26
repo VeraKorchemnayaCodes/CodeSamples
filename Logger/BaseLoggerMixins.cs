60f120b477482e1d0fb2753e6fdb01c1231bd627
Wvvern <84735487+Wvvern@users.noreply.github.com>
2023-01-25 22:51:32 -0800

    (== null) --> (is null)

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,28 +6,28 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null)
+        if (logger is null)
             throw new ArgumentNullException(nameof(logger));
         logger.Log(LogLevel.Error, string.Format(message, args));
     }
 
     public static void Warning(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null)
+        if (logger is null)
             throw new ArgumentNullException(nameof(logger));
         logger.Log(LogLevel.Warning, string.Format(message, args));
     }
 
     public static void Information(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null)
+        if (logger is null)
             throw new ArgumentNullException(nameof(logger));
         logger.Log(LogLevel.Information, string.Format(message, args));
     }
 
     public static void Debug(this BaseLogger logger, string message, params object[] args)
     {
-        if (logger == null)
+        if (logger is null)
             throw new ArgumentNullException(nameof(logger));
         logger.Log(LogLevel.Debug, string.Format(message, args));
     }
