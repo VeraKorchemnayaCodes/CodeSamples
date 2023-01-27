2cf7bf0b2d327a5e5686c396ccdbc05e906dfb24
Wvvern <84735487+Wvvern@users.noreply.github.com>
2023-01-26 20:25:45 -0800

    Init

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,18 @@
+﻿namespace CanHazFunny;
+
+public class Jester : IJokeService, IJokeOutput
+{
+    public Jester(JokeOutput output, JokeService service)
+    {
+        Output = output;
+        Service = service;
+    }
+
+    public JokeService Service { get; }
+    public JokeOutput Output { get; }
+
+    public void TellJoke()
+    {
+
+    }
+}
---
+++ CanHazFunny/CanHazFunny/JokeOutput.cs
@@ -0,0 +1,6 @@
+﻿namespace CanHazFunny;
+
+public class JokeOutput : IJokeOutput
+{
+
+}
