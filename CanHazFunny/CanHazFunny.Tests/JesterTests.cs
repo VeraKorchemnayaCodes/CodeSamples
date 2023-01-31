24ca659ebda4e8315c7221c5a8705f1e8eb11e52
Vera <vkorchemnaya@eagles.ewu.edu>
2023-01-30 16:27:36 -0800

    Simple Test Methods

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Constructor_GivenNullIJokeOutput_Throws()
+        {
+            IJokeService service = new Mock<IJokeService>().Object;
+            new Jester(null, service);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Constructor_GivenNullIJokeService_Throws()
+        {
+            IJokeOutput output = new Mock<IJokeOutput>().Object;
+            new Jester(output, null);
+        }
+
+        [TestMethod]
+        public void Constructor_GivenIJokeOutputAndIJokeService_Success()
+        {
+            IJokeService service = new Mock<IJokeService>().Object;
+            IJokeOutput output = new Mock<IJokeOutput>().Object;
+            new Jester(output, service);
+        }
+
+
+        [TestMethod]
+        public void TellJoke_Called_ReturnsJoke()
+        {
+            string tmp = "joke";
+            Mock<IJokeService> mock = new Mock<IJokeService>();
+            mock.Setup(JokeService => JokeService.GetJoke()).Returns("joke");
+            Assert.AreEqual<string>(tmp, mock.Object.GetJoke());
+        }
+
     }
 }
