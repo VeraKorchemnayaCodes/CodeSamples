e08a92d2705e835d08b295d0856caa03e88c4416
AndrewKorchemniy <andrewkorch@gmail.com>
2023-02-13 11:25:09 -0800

    Init

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericsHomework;
+
+public class Node<TNode>
+{
+    public Node(TNode value)
+    {
+        Value = value;
+    }
+
+    public TNode Value
+    {
+        get => _Value!;
+        set => _Value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public Node<TNode> Next
+    {
+        get => _Next!;
+        private set => _Next = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private Node<TNode>? _Next;
+    private TNode? _Value;
+
+    public override string ToString() => Value.ToString();
+}
---
+++ GenericsHomework/GenericsHomeworkTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GenericsHomeworkTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
