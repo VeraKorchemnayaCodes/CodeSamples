f8730a5c5f073f38b172c486ea70c365516c9b71
AndrewKorchemniy <andrewkorch@gmail.com>
2023-02-13 11:49:13 -0800

    Init

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericsHomework;
+
+public class Node<TNode> where TNode : struct
+{
+    public Node(TNode value)
+    {
+        Value = value;
+    }
+
+    public TNode Value { get; set; }
+    public Node<TNode> Next
+    {
+        get => _Next!;
+        private set => _Next = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private Node<TNode>? _Next;
+
+    public override string? ToString() => Value.ToString();
+}
