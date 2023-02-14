1a4a7dcae590b1d6fb4ec746a00ff72b130b1fc5
AndrewKorchemniy <andrewkorch@gmail.com>
2023-02-13 17:15:06 -0800

    Implented node methods

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,20 +1,51 @@
 ﻿namespace GenericsHomework;
 
-public class Node<TNode> where TNode : struct
+public class Node<T> where T : struct
 {
-    public Node(TNode value)
+    public Node(T value)
     {
         Value = value;
+        Next = this;
     }
 
-    public TNode Value { get; set; }
-    public Node<TNode> Next
+    public T Value { get; set; }
+    public Node<T> Next
     {
         get => _Next!;
         private set => _Next = value ?? throw new ArgumentNullException(nameof(value));
     }
 
-    private Node<TNode>? _Next;
+    private Node<T>? _Next;
 
-    public override string? ToString() => Value.ToString();
+    public void Append(T value)
+    {
+        if (Exists(value)) throw new ArgumentException("Value already exists.");
+        Node<T> newNode = new(value) { Next = this.Next };
+        Next = newNode;
+    }
+
+    public void Clear()
+    {
+        Next = this;
+    }
+
+    /*
+     * Setting next to itself is sufficient because there is no longer a root reference to the deleted nodes.
+     * It's irrelevant whether or not the deleted nodes point to eachother because they're no longer accessible by the code. 
+     * The garbage collector wont be able to find any pointers to the deleted nodes, making it sufficient.
+     */
+
+    public bool Exists(T value)
+    {
+        Node<T> currentNode = this.Next;
+        while (currentNode != this)
+        {
+            if (currentNode.Value.Equals(value))
+                return true;
+            currentNode = currentNode.Next;
+        }
+        return false;
+    }
+
+    public override string ToString() => Value.ToString() ?? "null";
 }
--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -1,10 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GenericsHomeworkTests;
 
 [TestClass]
 public class NodeTests
 {
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
+
 }
