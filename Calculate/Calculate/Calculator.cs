b2a3e9315bb5f90731f0dde061c58db27695478c
Vera <vkorchemnaya@eagles.ewu.edu>
2023-02-18 11:19:28 -0800

    Calculate class rough draft

---
+++ Calculate/Calculate/Calculator.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculate;
+
+public class Calculator
+{
+    public IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations {get => _MathematicalOperations;} 
+    Dictionary<char, Func<int, int, int>> _MathematicalOperations =
+            new Dictionary<char, Func<int, int, int>>
+            {
+                ['+'] = Add,
+                ['-'] = Subtract,
+                ['*'] = Multiple,
+                ['/'] = Divide
+            };
+
+    public static int Add(int parameter1, int parameter2)
+    {
+        return 0;
+    }
+    public static int Subtract(int parameter1, int parameter2)
+    {
+        return 0;
+    }
+
+    public static int Multiple(int parameter1, int parameter2)
+    {
+        return 0;
+    }
+
+    public static int Divide(int parameter1, int parameter2)
+    {
+        return 0;
+    }
+}
